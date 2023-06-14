917d79627fac9ac4c8252a595156be8ce094843b
ASUS <ASUS@DESKTOP-MG39ROF>
2023-06-13 20:30:17 -0500

    todos mis ejercicios

---
+++ src/ec/edu/Intsuperior/Controlador/Controlador.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.Intsuperior.Controlador;
+
+import ec.edu.Intsuperior.Modelo.Estudiante;
+import ec.edu.Intsuperior.Modelo.Profesor;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Controlador {
+    Estudiante e;
+    Profesor p;
+    
+    public void menu (){
+    
+    }
+    
+    
+}
