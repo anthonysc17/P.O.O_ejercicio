a696cb10b474a0011a20516b02fe895553e12256
ASUS <ASUS@DESKTOP-MG39ROF>
2023-06-14 19:48:31 -0500

    todos mis ejercicios

--- src/ec/edu/Intsuperior/Controlador/Controlador.java
+++ src/ec/edu/Intsuperior/Controlador/Controlador.java
@@ -4,22 +4,107 @@
  * and open the template in the editor.
  */
 package ec.edu.Intsuperior.Controlador;
-
-import ec.edu.Intsuperior.Modelo.Estudiante;
-import ec.edu.Intsuperior.Modelo.Profesor;
+import ec.edu.Intsuperior.Modelo.Cliente;
+import java.util.ArrayList;
 import javax.swing.JOptionPane;
 
 /**
  *
- * @author ASUS
+ * @author Anthony Sanchez
  */
 public class Controlador {
-    Estudiante e;
-    Profesor p;
-    
-    public void menu (){
-    
+  Cliente c;
+    ArrayList<Cliente> listaCliente = new ArrayList<Cliente>();
+
+    public void menu() {
+        String op;
+        do {
+            op = JOptionPane.showInputDialog("Ingresa una opción:\n"
+                    + "1.- Ingresar Cliente\n"
+                    + "2.- Consultar Clientes por cedula\n"
+                    + "3.- Actualizar Cliente\n"
+                    + "4.- Eliminar Cliente\n"
+                    + "5.- Salir");
+            switch (op) {
+                case "1":
+                    this.ingresarCliente();
+                    break;
+                case "2":
+                    this.consultarPorCedula(JOptionPane.showInputDialog("ingresa la cedula que estas buscando"));
+                    break;
+                case "3":
+                    this.actualizarCliente(JOptionPane.showInputDialog("ingresa la cedula que se va actualizar"));
+                    break;
+                case "4":
+                    this.eliminar(JOptionPane.showInputDialog("ingresa la cedula que se va eliminar"));
+                    break;
+            }
+
+        } while (!op.equals("5"));
+    }
+
+    public void ingresarCliente() {
+        c = new Cliente();
+        c.setCi(JOptionPane.showInputDialog("ingresa la cedula"));
+        c.setApellido1(JOptionPane.showInputDialog("ingresa apellido1"));
+        c.setApellido2(JOptionPane.showInputDialog("ingresa apellido2"));
+        c.setNombre1(JOptionPane.showInputDialog("ingresa nombre1"));
+        c.setNombre2(JOptionPane.showInputDialog("ingresa nombre2"));
+        c.setCorreo(JOptionPane.showInputDialog("Ingresa un correo"));
+        c.setCelular(JOptionPane.showInputDialog("Ingresa la celular"));
+
+        listaCliente.add(c);
+    }
+
+    public void cosultarTodosClientes() {
+        JOptionPane.showMessageDialog(null, listaCliente.toString());
+    }
+
+    public void consultarPorCedula(String ced) {
+        for (Cliente cliente : listaCliente) {
+            if (cliente.getCi().equals(ced)) {
+                JOptionPane.showMessageDialog(null, cliente.toString());
+
+            }
+
+        }
+    }
+
+    public void actualizarCliente(String ci) {
+        String op;
+        for (Cliente cliente : listaCliente) {
+            if (cliente.getCi().equals(ci)) {
+                do {
+                    op = JOptionPane.showInputDialog("Indica el parametro a actualizar\n"
+                            + "a.- Cedula\n"
+                            + "b.- Apellido1\n"
+                            + "c.- Apellido2\n"
+                            + "d.- Nombre1\n"
+                            + "e.- Nombre2\n"
+                            + "f.- Dirección\n"
+                            + "s.- Salir");
+                    switch (op) {
+                        case "a":
+                            cliente.setCi(JOptionPane.showInputDialog("ingresa nueva cedula"));
+                            break;
+                        case "b":
+                            cliente.setApellido1(JOptionPane.showInputDialog("ingresa nuevo apellido"));
+                            break;
+                    }
+                } while (!op.equals("s"));
+            } else {
+                JOptionPane.showMessageDialog(null, "Cliente no existe");
+            }
+        }
+
     }
     
-    
+    public void eliminar(String ced){
+        for (Cliente cliente : listaCliente) {
+            if(cliente.getCi().equals(ced)){
+                listaCliente.remove(cliente);
+            }
+            
+        }
+    }
 }
--- src/ec/edu/Intsuperior/Modelo/Persona.java
+++ src/ec/edu/Intsuperior/Modelo/Persona.java
@@ -5,60 +5,50 @@
  */
 package ec.edu.Intsuperior.Modelo;
 
-import java.util.Date;
-
 /**
  *
- * @author ASUS
+ * @author Anthony Sanchez
  */
 public class Persona {
-    private String cedula;
-    private String nombre1;
-    private String nombre2;
+      private String ci;
     private String apellido1;
     private String apellido2;
+    private String nombre1;
+    private String nombre2;
     private String direccion;
-    private String telefono;
     private String correo;
-    private Date fechaNacimiento;
+    private String celular;
+    private String fechaNacimiento;
 
     public Persona() {
     }
 
-    public Persona(String cedula, String nombre1, String nombre2, String apellido1, String apellido2, String direccion, String telefono, String correo, Date fechaNacimiento) {
-        this.cedula = cedula;
-        this.nombre1 = nombre1;
-        this.nombre2 = nombre2;
+    public Persona(String ci, String apellido1, String apellido2, String nombre1, String nombre2, String direccion, String correo, String celular, String fechaNacimiento) {
+        this.ci = ci;
         this.apellido1 = apellido1;
         this.apellido2 = apellido2;
+        this.nombre1 = nombre1;
+        this.nombre2 = nombre2;
         this.direccion = direccion;
-        this.telefono = telefono;
         this.correo = correo;
+        this.celular = celular;
         this.fechaNacimiento = fechaNacimiento;
     }
 
-    public String getCedula() {
-        return cedula;
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
     }
 
-    public void setCedula(String cedula) {
-        this.cedula = cedula;
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
     }
 
-    public String getNombre1() {
-        return nombre1;
+    public String getCi() {
+        return ci;
     }
 
-    public void setNombre1(String nombre1) {
-        this.nombre1 = nombre1;
-    }
-
-    public String getNombre2() {
-        return nombre2;
-    }
-
-    public void setNombre2(String nombre2) {
-        this.nombre2 = nombre2;
+    public void setCi(String ci) {
+        this.ci = ci;
     }
 
     public String getApellido1() {
@@ -77,20 +67,28 @@ public class Persona {
         this.apellido2 = apellido2;
     }
 
+    public String getNombre1() {
+        return nombre1;
+    }
+
+    public void setNombre1(String nombre1) {
+        this.nombre1 = nombre1;
+    }
+
+    public String getNombre2() {
+        return nombre2;
+    }
+
+    public void setNombre2(String nombre2) {
+        this.nombre2 = nombre2;
+    }
+
     public String getDireccion() {
         return direccion;
     }
 
     public void setDireccion(String direccion) {
         this.direccion = direccion;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
     }
 
     public String getCorreo() {
@@ -101,13 +99,25 @@ public class Persona {
         this.correo = correo;
     }
 
-    public Date getFechaNacimiento() {
-        return fechaNacimiento;
+    public String getCelular() {
+        return celular;
     }
 
-    public void setFechaNacimiento(Date fechaNacimiento) {
-        this.fechaNacimiento = fechaNacimiento;
+    public void setCelular(String celular) {
+        this.celular = celular;
     }
+
+    @Override
+    public String toString() {
+        return "\n\nDatos: \n"
+                + "Cédula: "+getCi()+"\n"
+                + "Apellidos: "+getApellido1()+" "+getApellido2()+"\n"
+                + "Nombres: "+getNombre1()+" "+getNombre2()+"\n"
+                + "Dirección: "+getDireccion()+"\n"
+                + "Correo: "+getCorreo()+"\n"
+                + "Celular: "+getCelular();               }
+     
     
     
 }
+    
